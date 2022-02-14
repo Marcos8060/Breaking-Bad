37258069dc3471e8a52fcc5c120241fe9118918b
Marcos8060 <marcosgav80@gmail.com>
2022-02-14 21:58:51 +0300

    fetched character details from the api and displayed all their details

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import Header from './Components/ui/Header';
+import CharacterGrid from './Components/characters/CharacterGrid';
+import { useState,useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [items,setItems] = useState([])
+  const [isLoading,setIsLoading] = useState(true)
+
+  useEffect(() =>{
+    const fetchItems = async()=>{
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+
+      console.log(result.data);
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItems();
+  },[])
+
   return (
     <div className="container">
-      <h1>Breaking Bad</h1>
+      <Header />
+      <CharacterGrid isLoading={isLoading} items={items}/>
     </div>
   );
 }
