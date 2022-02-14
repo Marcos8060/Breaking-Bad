b05bc4d77d1d8521e523938b7856a609141e2940
Marcos8060 <marcosgav80@gmail.com>
2022-02-14 22:38:01 +0300

    added the search functionality and spinner

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import './App.css';
 import Header from './Components/ui/Header';
 import CharacterGrid from './Components/characters/CharacterGrid';
+import Search from './Components/ui/Search';
 import { useState,useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
   const [items,setItems] = useState([])
   const [isLoading,setIsLoading] = useState(true)
+  const [query,setQuery] = useState('')
 
   useEffect(() =>{
     const fetchItems = async()=>{
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
 
       console.log(result.data);
       setItems(result.data)
       setIsLoading(false)
     }
     fetchItems();
-  },[])
+  },[query])
 
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(q) => setQuery(q)}/>
       <CharacterGrid isLoading={isLoading} items={items}/>
     </div>
   );
